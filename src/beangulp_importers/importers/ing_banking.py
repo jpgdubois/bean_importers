e04811e115d917760f2fc33ad4fa8f40ccf7c36d
Justin Dubois <r37ja8kf@jpgdubois.anonaddy.com>
2024-10-01 22:27:25 +0200

    feat: add ing savings importer

--- src/beangulp_importers/importers/ing_banking.py
+++ src/beangulp_importers/importers/ing_banking.py
@@ -11,7 +11,7 @@ from beangulp_importers.datatypes import TransactionType, Currency
 from beangulp_importers.base_importer.banking_importer import BankingImporter
 
 
-class INGImporter(BankingImporter):
+class INGCurrentImporter(BankingImporter):
     
     def __init__(
         self,
@@ -37,7 +37,7 @@ class INGImporter(BankingImporter):
             transaction_type_key="Code",
             transaction_type_mapping={
                 TransactionType.exchange: [],
-                TransactionType.transfer: ["OV", "GT", "VZ", "ID", "GT", "IC", "GT", "OV"],
+                TransactionType.transfer: ["OV", "GT", "VZ", "ID", "GT", "IC", "GT", "OV", "BA"],
                 TransactionType.skip: [],
             },
         )
@@ -68,11 +68,69 @@ class INGImporter(BankingImporter):
         )
 
 
+class INGSavingsImporter(BankingImporter):
+    
+    def __init__(
+        self,
+        root_account: str,
+        root_account_id: str,
+    ):
+
+        file_description = file.FileDescriptionCSV(
+            file_extension = ".csv",
+            file_pattern_regex = rf"^{root_account_id}_(?P<date>\d{{2}}-\d{{2}}-\d{{4}})_\d{{2}}-\d{{2}}-\d{{4}}\.csv$",
+            file_date_format = "%d-%m-%Y",
+            file_delimiter = ";",
+            file_header = ("Datum", "Omschrijving", "Rekening", "Rekening naam", "Tegenrekening", "Af Bij", "Bedrag", "Valuta", "Mutatiesoort", "Mededelingen", "Saldo na mutatie"),
+            entry_mapping=None,
+            start_date = None,
+            end_date = None,
+        )
+        get_date = date.FromDate(
+            date="Datum",
+            date_format="%Y-%m-%d",
+        )
+        get_transaction_type = transaction_type.FromTransactionType(
+            transaction_type_key="Mutatiesoort",
+            transaction_type_mapping={
+                TransactionType.exchange: [],
+                TransactionType.transfer: ["Inleg"],
+                TransactionType.skip: [],
+            },
+        )
+        get_payee_narration = payee_narration.FromPayeeNarration(
+            payee_key=r"Tegenrekening",
+            max_payee_length=20,
+            narration_key=r"Mededelingen",
+            max_narration_length=40,
+        )
+        get_amount = amount.FromSignAmountWithCurrency(
+            sign_key=r"Af Bij",
+            amount_key="Bedrag",
+            currency=Currency.EUR,
+        )
+        get_balance = amount.FromAmountWithCurrency(
+            amount_key="Saldo na mutatie",
+            currency=Currency.EUR,
+        )
+        BankingImporter.__init__(
+            self,
+            root_account=root_account,
+            fee_account=None,
+            get_date=get_date,
+            get_payee_narration=get_payee_narration,
+            get_transaction_type=get_transaction_type,
+            get_root_amount=get_amount,
+            get_fee_amount=None,
+            get_balance=get_balance,
+            file_description=file_description,
+        )
+
 if __name__ == "__main__":
     main = beangulp.Ingest([
-        INGImporter(
-            root_account="Assets:Revolut:EUR",
-            root_account_id="NL00INGB0000000000",
+        INGSavingsImporter(
+            root_account="Assets:NL:ING:Current",
+            root_account_id="",
         ),
     ])
     main()
